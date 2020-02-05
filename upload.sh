c4da27870d23d38ae63782c5ceb732df257935c5
alvarohm <alvaro.miyazawa@york.ac.uk>
2020-02-05 11:31:24 +0000

    Update upload.sh
    Changed remote directory for rsync to reflect changes in the structure of the server. The folder changed from webcs to www. This needs to be done to all upload.sh scripts. I'll test on this repository and then change the others.
    
    ps.: Authorised keys also need to be updated if they use full commands.

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 echo "Get current version"
 dir=circus.robocalc.robochart.update/target/repository/
-remote=/shared/storage/cs/webcs/robostar/robotool/metamodel/
+remote=/shared/storage/cs/www/robostar/robotool/metamodel/
 url=ahm504@sftp.york.ac.uk
 file=$(ls $dir/features | grep -m 1 jar)
 version=${file#*_}
